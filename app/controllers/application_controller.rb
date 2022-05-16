bf2e7b83fd189cf6b6a0816b2a31154c0931c64a
orozcoding <orozcoding@gmail.com>
2022-05-16 07:09:19 -0400

    Create temporal current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user = User.find(1)
+    @current_user
+  end
+
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -64,6 +64,8 @@ class RecipesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    recipe_hash = params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    recipe_hash[:user] = current_user
+    recipe_hash
   end
 end
