1c59876499317255f2b6f4568abe48fddeb78ff2
orozcoding <orozcoding@gmail.com>
2022-05-19 21:14:49 -0400

    Delete inventory files

--- spec/model/recipe_spec.rb
+++ spec/model/recipe_spec.rb
@@ -1,35 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  describe 'recipe model' do
-    subject { Recipe.create(name: 'apple jam', preparation_time: 5, cooking_time: 4, description: 'hello', user_id: 1) }
+  describe 'Recipe model' do
+    before(:example) do
+      User.destroy_all
+      Recipe.destroy_all
+      User.create(id: 1, name: 'Jones', email: 'email@dot.com', password: '321321')
+      @recipe = Recipe.create(name: 'apple jam', preparation_time: 5, cooking_time: 4, description: 'hello', user_id: 1)
+    end
 
-    it 'check if subject name is null' do
-      subject.name = nil
-      expect(subject).to_not be_valid
+    it 'check if recipe name is null' do
+      @recipe.name = nil
+      expect(@recipe).to_not be_valid
     end
 
     it 'check if model is valid ' do
-      subject.name = 'Orange'
-      expect(subject).to be_valid
+      @recipe.name = 'Orange'
+      expect(@recipe).to be_valid
     end
 
     it 'check if model is invalid ' do
-      subject.preparation_time = 'Orange'
-      expect(subject).to_not be_valid
+      @recipe.preparation_time = 'Orange'
+      expect(@recipe).to_not be_valid
     end
 
     it 'check if model is invalid ' do
-      subject.cooking_time = 'anple'
-      expect(subject).to_not be_valid
+      @recipe.cooking_time = 'anple'
+      expect(@recipe).to_not be_valid
     end
 
-    it 'check if model name is invalid ' do
-      subject.cooking_time = "anpleanpleanpleanpleanpleanpleanpleanple
-        anpleanpleanpleanpleanpleanpleanpleanpleanpleanpleanpleanpleanpleanpleanpleanpleanple
-        anpleanpleanpleanpleanple
-        anpleanpleanpleanpleanpleanpleanpleanpleanple"
-      expect(subject).to_not be_valid
+    it 'check if model is valid ' do
+      @recipe.preparation_time = 52
+      expect(@recipe).to be_valid
+    end
+
+    it 'check if model is valid ' do
+      @recipe.cooking_time = 32
+      expect(@recipe).to be_valid
     end
   end
 end
